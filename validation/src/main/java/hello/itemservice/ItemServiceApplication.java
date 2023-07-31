25c370514eaedbc1fa2ae562fec699c5297fdbe8
JinSung-Hwang <wlstjd7782@gmail.com>
2023-08-01 00:07:19 +0900

    feature: 섹션4 - validator 분리 1~2

--- validation/src/main/java/hello/itemservice/ItemServiceApplication.java
+++ validation/src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -1,13 +1,23 @@
 package hello.itemservice;
 
+import hello.itemservice.web.validation.ItemValidator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.validation.Validator;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 
 @SpringBootApplication
-public class ItemServiceApplication {
+public class ItemServiceApplication implements WebMvcConfigurer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ItemServiceApplication.class, args);
 	}
 
+//	@Override
+//	public Validator getValidator() { // validator global 적용이다.
+   	  // controller에는 @Validated 라는 어노테이션을 붙어있어야한다.
+	    // 하나의 validator를 global로 사용하는일은 잘 없을거 같다.
+//		return new ItemValidator();
+//	}
 }
