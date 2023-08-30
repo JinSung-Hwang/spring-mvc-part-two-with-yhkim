4f3859a55881018dd38423ef1dbf4305e777e5bf
JinSung-Hwang <wlstjd7782@gmail.com>
2023-08-30 23:18:08 +0900

    feature: 섹션6 - 로그인 처리 쿠키 세션 - 쿠키 사용

--- login/src/main/java/hello/login/web/HomeController.java
+++ login/src/main/java/hello/login/web/HomeController.java
@@ -1,15 +1,50 @@
 package hello.login.web;
 
+import hello.login.domain.member.Member;
+import hello.login.domain.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Slf4j
 @Controller
+@RequiredArgsConstructor
 public class HomeController {
+    private final MemberRepository memberRepository;
 
-    @GetMapping("/")
+//    @GetMapping("/")
     public String home() {
         return "home";
     }
+
+    // required = false로 주는 이유는 로그인 안된 사용자도 들어와야 하기 때문이다.
+    // memberId는 String이지만 Long으로 줘도 typeConvert되어서 넘어온다.
+    @GetMapping("/")
+   public String homeLogin(@CookieValue(name = "memberId", required = false) Long memberId, Model model) {
+        if (memberId == null) {
+            return "home";
+        }
+
+        //로그인
+        Member loginMember = memberRepository.findById(memberId);
+        if (loginMember == null) {
+            return "home";
+        }
+
+        model.addAttribute("member", loginMember);
+        return "loginHome";
+    }
 }
+
+
+
+
+
+
+
+
+
+
--- login/src/main/java/hello/login/web/login/LoginController.java
+++ login/src/main/java/hello/login/web/login/LoginController.java
@@ -4,13 +4,15 @@ import hello.login.domain.login.LoginService;
 import hello.login.domain.member.Member;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 
 @Slf4j
 @Controller
@@ -24,7 +26,7 @@ public class LoginController {
   }
 
   @PostMapping("/login")
-  public String Login(@Validated @ModelAttribute LoginForm form, BindingResult bindingResult) {
+  public String login(@Validated @ModelAttribute LoginForm form, BindingResult bindingResult, HttpServletResponse response) {
     if (bindingResult.hasErrors()) {
       return "login/loginForm";
     }
@@ -37,6 +39,22 @@ public class LoginController {
 
     // 로그인 성공 처리 TODO
 
+    // 쿠키에 시간 정보를 주지 않으면 세션 쿠키(브라우저 종료시 모두 종료)
+    Cookie idCookie = new Cookie("memberId", String.valueOf(loginMember.getId()));
+    response.addCookie(idCookie);
+
+    return "redirect:/";
+  }
+
+  @PostMapping("/logout")
+  public String logout(HttpServletResponse response) {
+    return expireCookie(response, "memberId");
+  }
+
+  private static String expireCookie(HttpServletResponse response, String cookieName) {
+    Cookie cookie = new Cookie(cookieName, null);
+    cookie.setMaxAge(0);
+    response.addCookie(cookie);
     return "redirect:/";
   }
 }
