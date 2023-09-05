4e5538d45309440e7365ad6b0711b77174e98c88
JinSung-Hwang <wlstjd7782@gmail.com>
2023-09-06 00:49:03 +0900

    feature: 섹션6 - 로그인 처리 쿠키 세션 - 서블릿 HTTP 세션2

--- login/src/main/java/hello/login/web/HomeController.java
+++ login/src/main/java/hello/login/web/HomeController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
@@ -57,13 +58,34 @@ public class HomeController {
     return "loginHome";
   }
 
+//  @GetMapping("/")
+//  public String homeLoginV3(HttpServletRequest request, Model model) {
+//    HttpSession session = request.getSession(false);
+//    if (session == null) {
+//      return "home";
+//    }
+//    Member loginMember = (Member) session.getAttribute(SessionConst.LOGIN_MEMBER);
+//
+//    // 세션에 회원 데이터가 없으면 home
+//    if (loginMember == null) {
+//      return "home";
+//    }
+//
+//    // 세션이 유지되면 로그인으로 이동
+//    model.addAttribute("member", loginMember);
+//    return "loginHome";
+//  }
+
   @GetMapping("/")
-  public String homeLoginV3(HttpServletRequest request, Model model) {
-    HttpSession session = request.getSession(false);
-    if (session == null) {
-      return "home";
-    }
-    Member loginMember = (Member) session.getAttribute(SessionConst.LOGIN_MEMBER);
+  public String homeLoginV3Spring(@SessionAttribute(name = SessionConst.LOGIN_MEMBER, required = false) Member loginMember, Model model) {
+    // @SessionAttribute는 세션을 생성하지 않기때문에 세션을 찾을때만 사용한다.
+
+    // 아래 4줄 로직이 모두 @SessionAttribute로 모두 처리 된다.
+//    HttpSession session = request.getSession(false);
+//    if (session == null) {
+//      return "home";
+//    }
+//    Member loginMember = (Member) session.getAttribute(SessionConst.LOGIN_MEMBER);
 
     // 세션에 회원 데이터가 없으면 home
     if (loginMember == null) {
