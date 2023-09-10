41d28980899fbfb15cbea9755ab04b5dfa6823c2
JinSung-Hwang <wlstjd7782@gmail.com>
2023-09-10 18:32:00 +0900

    feature: 섹션7 - 로그인 처리2 - 스프링 인터셉터 요청 로그

--- login/src/main/java/hello/login/WebConfig.java
+++ login/src/main/java/hello/login/WebConfig.java
@@ -2,16 +2,28 @@ package hello.login;
 
 import hello.login.web.filter.LogFilter;
 import hello.login.web.filter.LoginCheckFilter;
+import hello.login.web.interceptor.LogInterceptor;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import javax.servlet.Filter;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig {
+public class WebConfig implements WebMvcConfigurer {
 
-  @Bean
+  @Override
+  public void addInterceptors(InterceptorRegistry registry) {
+    registry.addInterceptor(new LogInterceptor())
+      .order(1)
+      .addPathPatterns("/**") // **는 하위 경로까지 모두 적용한다는 뜻이다. *는 현재 경로만 적용한다는 뜻이다.
+      .excludePathPatterns("/css/**", "/*.ico", "/error"); // addPathPatterns로 모든 경로는 허용하지만 css, ico, error는 제외한다.
+      // interceptor는 filter와는 다르게 배제할 pathPattern도 지정할 수도 있고 ?, *, **, {spring} 등과 같은 패턴을 사용할 수 있다.
+  }
+
+//  @Bean
   public FilterRegistrationBean logFilter() {
     FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<Filter>();
     filterRegistrationBean.setFilter(new LogFilter());
@@ -32,7 +44,4 @@ public class WebConfig {
 
     return filterRegistrationBean;
   }
-
-
-
 }
---
+++ login/src/main/java/hello/login/web/interceptor/LogInterceptor.java
@@ -0,0 +1,62 @@
+package hello.login.web.interceptor;
+
+import java.util.UUID;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+@Slf4j
+public class LogInterceptor implements HandlerInterceptor {
+
+  public static final String LOG_ID = "logId";
+
+  @Override
+  public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+
+    String requestURI = request.getRequestURI();
+    String uuid = UUID.randomUUID().toString();
+
+    request.setAttribute(LOG_ID, uuid);
+
+    // 핸들러 정보는 어떤 핸들러 매핑을 사용하는가에 따라 달라진다.
+    // @RequestMapping, @Controller를 사용하면 HandlerMethod가 넘어온다.
+    // `/resources/static`와 같은 정적 리소스를 요청하면 ResourceHttpRequestHandler가 넘어온다.
+    if (handler instanceof HandlerMethod) {
+      // @RequestMapping: HandlerMethod // 컨트롤러 메서드에 @RequestMapping이 붙어있으면 HandlerMethod가 온다.
+      // 정적 리소스: ResourceHttpRequestHandler
+      HandlerMethod hm = (HandlerMethod) handler;// 호출한 컨트롤러 메서드의 모든 정보가 포함되어있다.
+    }
+
+    log.info("로그 인터셉터 REQUEST preHandle [{}][{}][{}]", uuid, requestURI, handler);
+
+    return true; // 리턴 false면 다음 핸들러 어댑터와 컨트롤러가 실행되지 않는다. true면 실행된다.
+  }
+
+  @Override
+  public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+    // controller에서 exception이 발생했으면 postHandle은 실행되지 않는다.
+
+
+    log.info("로그 인터셉터 RESPONSE postHandle [{}]", modelAndView);
+  }
+
+  @Override
+  public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+    // Exception은 컨트롤러에서 예외가 발생하지 않았다면 ex에는 null이 넘어온다.
+    // 종료 로그를 afterCompletion을 사용하는 이유는 예외가 발생했을 때도 호출되기 때문이다.
+
+    String requestURI = request.getRequestURI();
+    Object logId = (String) request.getAttribute(LOG_ID); // request는 한 세션 동안 같은 request 객체를 사용하기 때문에 logId를 저장해놓으면 다른 메서드에서도 사용할 수 있다.
+    // 즉 preHandle에서 저장한 logId를 afterCompletion을 사용할 수 있다.
+
+    log.info("로그 인터셉터 RESPONSE afterCompletion [{}][{}][{}]", logId, requestURI, handler);
+    if (ex != null) {
+      log.error("afterCompletion error!!", ex);
+    }
+
+    HandlerInterceptor.super.afterCompletion(request, response, handler, ex);
+  }
+}
