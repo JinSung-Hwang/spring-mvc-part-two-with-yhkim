eee4db5268e12b78d2679856b25006c3c80b3193
JinSung-Hwang <wlstjd7782@gmail.com>
2023-09-23 18:17:43 +0900

    feature: 섹션8 - 예외 처리와 오류 페이지 - 서블릿 예외 처리 - 인터셉터

--- exception/src/main/java/hello/exception/WebConfig.java
+++ exception/src/main/java/hello/exception/WebConfig.java
@@ -1,16 +1,30 @@
 package hello.exception;
 
 import hello.exception.filter.LogFilter;
+import hello.exception.interceptor.LogInterceptor;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.servlet.DispatcherType;
 import javax.servlet.Filter;
 
 @Configuration
-public class WebConfig {
-  @Bean
+public class WebConfig implements WebMvcConfigurer { // note: webMvcConfigurer는 interceptor를 등록할때 사용한다.
+
+  @Override
+  public void addInterceptors(InterceptorRegistry registry) {
+    registry.addInterceptor(new LogInterceptor())
+      .order(1)
+      .addPathPatterns("/**")
+      .excludePathPatterns("/css/**", "*.icon", "/error", "/error/page/**"); // 오류 페이지 경로
+      // note: interceptor는 dispatcherType을 가지고 request호출과 error호출을 구분하지 않는다.
+      // note: 대신에 excludePathPatterns를 가지고 error호출은 interceptor를 타지 않도록 만들 수 있다.
+  }
+
+//  @Bean
   public FilterRegistrationBean logFilter() {
     FilterRegistrationBean<Filter> filterRegistrationBean =new FilterRegistrationBean<>();
     filterRegistrationBean.setFilter(new LogFilter());
