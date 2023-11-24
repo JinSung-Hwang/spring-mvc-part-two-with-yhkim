132086deea652218f989f86fd78dc2037c97a326
JinSung-Hwang <wlstjd7782@gmail.com>
2023-11-24 23:20:35 +0900

    feature: 섹션10 - 스프링 타입 컨버터 - 스프링 타입 컨버터 소개

---
+++ typeconverter/src/main/java/hello/typeconverter/controller/HelloController.java
@@ -0,0 +1,24 @@
+package hello.typeconverter.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+  @GetMapping("/hello-v1")
+  public String helloV1(HttpServletRequest request) {
+    String data = request.getParameter("data"); // note: getParameter()의 return type은 String이다.
+    Integer intValue = Integer.valueOf(data); // note: 그래서 Integer 타입을 사용하려면 Integer.valueOf()를 사용하여 String을 Integer로 변환한다.
+    System.out.println("intValue = " + intValue);
+    return "ok";
+  }
+
+  @GetMapping("/hello-v2")
+  public String helloV2(@RequestParam Integer data) { // note: 하지만 스프링에서는 중간에 String을 Integer로 변환하는 과정을 처리해준다. 이것이 TypeConverter의 역할이다.
+    System.out.println("data = " + data);
+    return "ok";
+  }
+
+}
